b7b9129b507270518bb79564396cf2b2bd2be2f7
xuke918 <xuke3070@gmail.com>
2016-04-08 13:33:38 -0400

    Fixed the name and make them same name.

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ var $banner = $('.banner');
 var bannerWidth = $banner.outerWidth();
 var bannerHeight = $banner.outerHeight();
 var i = 0;
-var totalStars = 75;
+var stars = 75;
 var $newStar;
 
-for (i = 0; i < totalStars; i++) {
+for (i = 0; i < stars; i++) {
   $newStar = $('<div>');
   $newStar.addClass('star');
 
