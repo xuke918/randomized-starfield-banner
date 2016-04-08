d6cb58bb81c746ed08eb487aa5a7a3efbe1f359e
xuke918 <xuke3070@gmail.com>
2016-04-08 10:42:16 -0400

    Added stars number 75.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ var bannerWidth = $banner.outerWidth();
 var bannerHeight = $banner.outerHeight();
 
 var i = 0;
+var totalStars = 75;
+var $newStar;
+
 
 for (i = 0; i < stars; i++) {
   // For-loop
