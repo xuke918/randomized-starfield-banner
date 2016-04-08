572b1e93100f1da44ea0bc738ec30c12820893c7
xuke918 <xuke3070@gmail.com>
2016-04-08 10:59:55 -0400

    Fixed the empty line.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,19 @@
 var $banner = $('.banner');
-
 var bannerWidth = $banner.outerWidth();
 var bannerHeight = $banner.outerHeight();
-
 var i = 0;
-var Stars = 75;
+var totalStars = 75;
 var $newStar;
 
-for (i = 0; i < Stars; i++) {
+for (i = 0; i < totalStars; i++) {
   $newStar = $('<div>');
   $newStar.addClass('star');
+
   $newStar.css({
     top: Math.random() * bannerHeight,
     left: Math.random() * bannerWidth,
     transform: 'rotate(' + Math.random() * 360 + 'deg) scale(' + Math.random() + ')',
     opacity: Math.random()
   });
-  $banner.append($Stars);
+  $banner.append($newStars);
 }
